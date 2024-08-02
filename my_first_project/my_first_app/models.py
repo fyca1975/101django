95e9fcd5bd26f2a13cb66e2d1b4f844a12ee53b5
Fredy Yecid Castro Agray <fredy.castro@gmail.com>
2024-08-02 06:08:09 -0500

    incluir git ignore

--- my_first_project/my_first_app/models.py
+++ my_first_project/my_first_app/models.py
@@ -15,12 +15,35 @@ class Publisher (models.Model):
 
     def __str__(self) -> str:
         return self.name
+
     
+class Author(models.Model):
+    name = models.TextField(max_length=200)
+    birth_date =models.DateField()
+
+    def __str__(self) -> str:
+        return self.name
+    
+class Profile(models.Model):
+    author =models.OneToOneField(Author, on_delete=models.CASCADE)
+    website = models.URLField()
+    biografia = models.TextField(max_length=500)
+
+    
+
+
+
 class Book(models.Model):
     title =models.TextField(max_length=150)
     pubication_date = models.DateField()
     publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    authors = models.ManyToManyField(Author, related_name="authors")
 
     def __str__(self) -> str:
         return self.title    
+
+
+
+
+
 
